b9ee42792753223ecb0fae210396918a89eed7f4
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2023-07-22 14:48:46 +0200

    split project in files

--- bot.ts
+++ bot.ts
@@ -5,137 +5,22 @@ import { Wallet } from 'ethers'
 //@ts-ignore
 import qrcode from 'qrcode-terminal'
 import { request, gql } from 'graphql-request'
+import { GetTokenHolders } from './query';
+import { WelcomeMessage } from './messages';
 
-const wallet = new Wallet("");
+const wallet = new Wallet("464faef01d67d4124510cfb32aae7950899478208b90f56f2aae3da5806b9a5d");
+console.log("toto")
 
 qrcode.generate(`https://go.cb-w.com/messaging?address=${wallet?.address}`)
 
-const document = gql`
-query GetTokenHolders($limit: Int) {
-  ethereum: TokenBalances(
-    input: {filter: {tokenAddress: {_eq: "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"}}, blockchain: ethereum, limit: $limit}
-  ) {
-    TokenBalance {
-      tokenAddress
-      tokenId
-      blockchain
-      tokenType
-      token {
-        name
-        symbol
-        logo {
-          medium
-          small
-        }
-        projectDetails {
-          imageUrl
-        }
-      }
-      tokenNfts {
-        contentValue {
-          video
-          image {
-            small
-            original
-            medium
-            large
-            extraSmall
-          }
-        }
-      }
-      owner {
-        identity
-        addresses
-        socials {
-          blockchain
-          dappSlug
-          profileName
-        }
-        primaryDomain {
-          name
-        }
-        domains {
-          chainId
-          dappName
-          name
-        }
-        xmtp {
-          isXMTPEnabled
-        }
-      }
-    }
-    pageInfo {
-      nextCursor
-      prevCursor
-    }
-  }
-  polygon: TokenBalances(
-    input: {filter: {tokenAddress: {_eq: "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"}}, blockchain: polygon, limit: $limit}
-  ) {
-    TokenBalance {
-      tokenAddress
-      tokenId
-      blockchain
-      tokenType
-      token {
-        name
-        symbol
-        logo {
-          medium
-          small
-        }
-        projectDetails {
-          imageUrl
-        }
-      }
-      tokenNfts {
-        contentValue {
-          video
-          image {
-            small
-            original
-            medium
-            large
-            extraSmall
-          }
-        }
-      }
-      owner {
-        identity
-        addresses
-        socials {
-          blockchain
-          dappSlug
-          profileName
-        }
-        primaryDomain {
-          name
-        }
-        domains {
-          chainId
-          dappName
-          name
-        }
-        xmtp {
-          isXMTPEnabled
-        }
-      }
-    }
-    pageInfo {
-      nextCursor
-      prevCursor
-    }
-  }
-}
-`;
 run(async (context) => {
-  const data = await request('https://api.airstack.xyz/gql', document, { limit: 1, headers: {
+  const data = await request('https://api.airstack.xyz/gql', GetTokenHolders, { limit: 2, headers: {
     authorization: `Bearer MY_TOKEN`,
   }})
   console.dir(data, { depth: null })
   // When someone sends your bot a message, you can get the DecodedMessage
   // from the HandlerContext's `message` field
-  await context.reply(`Howdy partner! How can I help you today?`);
+  await context.reply(WelcomeMessage);
 
   const messageBody = context.message.content;
 
---
+++ messages.ts
@@ -0,0 +1,3 @@
+export const WelcomeMessage = "Welcome to the Token Terminal GraphQL API! \n \
+  I am here to help you find NFTs and contact their owners. \n \
+  If you want to start type NFT"
